65141953e08142ab3d31512966952c078e05492a
Rurangwa <chrisruu@yahoo.com>
2023-10-01 19:55:51 +0800

    updates

--- src/main/java/com/codelab/WorldNewsApp/service/NewsServiceImpl.java
+++ src/main/java/com/codelab/WorldNewsApp/service/NewsServiceImpl.java
@@ -2,12 +2,10 @@ package com.codelab.WorldNewsApp.service;
 
 import com.codelab.WorldNewsApp.model.News;
 import com.google.gson.JsonObject;
-import org.springframework.stereotype.Service;
-import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+import org.springframework.stereotype.Service;
 
 
 @Service
@@ -17,8 +15,8 @@ public class NewsServiceImpl implements NewsService {
     public JsonObject getNews(News news) {
 
         String apiKey = "f20248dbdbb54fd791f0c8d8b531f894";
-        String apiUrl = "https://newsapi.org/v2/top-headlines?country="+news.getCountry(); // You can customize the API URL as needed
-        JsonObject newResponse = fetchNews(apiKey,apiUrl,news);
+        String apiUrl = "https://newsapi.org/v2/top-headlines?country=" + news.getCountry(); // You can customize the API URL as needed
+        JsonObject newResponse = fetchNews(apiKey, apiUrl, news);
 
         return newResponse;
     }
@@ -30,6 +28,14 @@ public class NewsServiceImpl implements NewsService {
 
         HttpGet httpGet = new HttpGet(apiUrl);
         httpGet.addHeader("X-Api-Key", apiKey);
+
+        try {
+            ...
+        }catch (Exception e){
+
+
+        }
+
         return null;
     }
 }
